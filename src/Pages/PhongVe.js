340280373fc2f04d32c8757b0bd82569f8846a59
Trung Lư <lutrung>
2020-11-17 20:14:09 +0700

    'finish'

--- src/Pages/PhongVe.js
+++ src/Pages/PhongVe.js
@@ -1,24 +1,80 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { layThongTinPhongVeApiAction } from '../redux/actions/QuanLyPhimAction';
+import { datVeApiAction, layThongTinPhongVeApiAction } from '../redux/actions/QuanLyPhimAction';
+import { USER_LOGIN } from '../Util/Config';
 
 export default function PhongVe(props) {
-    const thongTinPhongVe = useSelector(state=> state.QuanLyPhimReducer.thongTinPhongVe)
+    const thongTinPhongVe = useSelector(state => state.QuanLyPhimReducer.thongTinPhongVe)
+    const danhSachGheDangDat = useSelector(state => state.QuanLyPhimReducer.danhSachGheDangDat)
+    console.log(danhSachGheDangDat)
     const dispatch = useDispatch();
     useEffect(async () => {
         // lasy tham so lich chieu tu url
         let maLichChieu = props.match.params.maLichChieu;
         // goi action ket noi api lay du lieu lich chieu ve
-        dispatch( await layThongTinPhongVeApiAction(maLichChieu))
+        dispatch(await layThongTinPhongVeApiAction(maLichChieu))
     }, [])
     return (
-        <div className='container'>
+        <div className='container-fluid'>
             <div className='row'>
-                <div className='col-8'>
+                <div className='col-8 text-center'>
                     {/* hien thi thong tin phong ve */}
+                    <div className='manHinh'>
+                        <img src='https://tix.vn/app/assets/img/icons/screen.png' />
+                    </div>
+                    <div className='danhSachGhe'>
+                        {thongTinPhongVe.danhSachGhe?.map((ghe, index) => {
+                            let classGheVip = ghe.loaiGhe === 'Thuong' ? '' : 'gheVip';
+                            let classGheDaDat = ghe.daDat ? 'gheDaDat' : '';
+                            let disable = ghe.daDat ? 'disable' : '';
+                            let noiDungBtn = ghe.daDat ? 'X' : ghe.stt;
+                            let indexGheDangDat = danhSachGheDangDat.findIndex(gheDangDat => ghe.maGhe === gheDangDat.maGhe)
+                            let classGheDangDat = indexGheDangDat !== -1 ? 'gheDangDat' : '';
+                            return <Fragment key={index}>
+                                <button onClick={() => {
+                                    dispatch({
+                                        type: "DAT_GHE",
+                                        gheDangDat: {
+                                            maGhe: ghe.maGhe,
+                                            giaVe: ghe.giaVe,
+                                            stt: ghe.stt
+                                        }
+                                    })
+                                }} disabled={`${disable}`} className={`ghe ${classGheVip} ${classGheDaDat} ${classGheDangDat}`}>{noiDungBtn}</button>
+                                {(index + 1) % 16 === 0 ? <br /> : ''}
+                            </Fragment>
+                        })}
+                    </div>
                 </div>
                 <div className='col-4'>
                     {/* hien thi thong tin phim */}
+                    <div className='display-4 text-center'>{danhSachGheDangDat.reduce((tongTien, gheDangDat, index) => {
+                        return tongTien += gheDangDat.giaVe
+                    }, 0).toLocaleString()} VND</div>
+                    <hr />
+                    <h1>{thongTinPhongVe.thongTinPhim?.tenPhim}</h1>
+                    <img src={thongTinPhongVe.thongTinPhim?.hinhAnh} style={{ width: 180, height: 200 }} alt='' />
+                    <h5>{thongTinPhongVe.thongTinPhim?.tenCumRap} - {thongTinPhongVe.thongTinPhim?.tenRap}</h5>
+                    <p>{thongTinPhongVe.thongTinPhim?.ngayChieu} - {thongTinPhongVe.thongTinPhim?.gioChieu}</p>
+                    <hr />
+                    <div>Ghế: {danhSachGheDangDat?.map((gheDangDat, index) => {
+                        return <span key={index} className='mx-1'>{gheDangDat.stt}</span>
+                    })}</div>
+                    <hr />
+                    <button className='btn btn-success w-100' onClick={async () => {
+                        if (localStorage.getItem(USER_LOGIN)) {
+                            let user = JSON.parse(localStorage.getItem(USER_LOGIN))
+                            let objectDatVe = {
+                                'maLichChieu': props.match.params.maLichChieu,
+                                'danhSachVe': danhSachGheDangDat,
+                                'taiKhoanNguoiDung': user.taiKhoan
+                            }
+                            dispatch(await datVeApiAction(objectDatVe))
+                        } else {
+                            props.history.push('/dangnhap')
+                        }
+
+                    }}>Đặt vé</button>
                 </div>
             </div>
         </div>
--- src/redux/actions/QuanLyPhimAction.js
+++ src/redux/actions/QuanLyPhimAction.js
@@ -1,5 +1,7 @@
 import Axios from 'axios'
-import { LAY_DANH_SACH_PHIM_ACTION,LAY_CHI_TIET_PHIM_ACTION, THONG_TIN_PHONG_VE_ACTION } from '../const/QuanLyPhimConst'
+import Swal from 'sweetalert2';
+import { ACCESSTOKEN } from '../../Util/Config';
+import { LAY_DANH_SACH_PHIM_ACTION, LAY_CHI_TIET_PHIM_ACTION, THONG_TIN_PHONG_VE_ACTION } from '../const/QuanLyPhimConst'
 // Action gọi API (không dispatch dữ liệu trực tiếp lên reducer)
 export const layDanhSachPhimAPIAction = () => {
     return dispatch => {
@@ -25,7 +27,7 @@ export const layDanhSachPhimAPI = (dataPhim) => {
 }
 
 // -------------------- CÁCH VIẾT KHÁC Ở TRÊN (CHẠY TỪ TRÊN XUỐNG DƯỚI CÒN CÁCH Ở TRÊN LÀ CHẠY BẤT ĐỒNG BỘ) ---------
-export const layChiTietPhimApiAction = (maPhim) => {
+export const layChiTietPhimApiAction = async (maPhim) => {
     return async (dispatch) => {
         try {
             // Gọi api lấy dữ liệu chi tiết phim về
@@ -49,16 +51,16 @@ export const layChiTietPhimApiAction = (maPhim) => {
 
 
 // lay thong tin phong ve-------------------------------
-export const layThongTinPhongVeApiAction = (maLichChieu) => {
+export const layThongTinPhongVeApiAction = async (maLichChieu) => {
     return async (dispatch) => {
         try {
-            let {data,status} = await Axios({
+            let { data, status } = await Axios({
                 url: `https://movie0706.cybersoft.edu.vn/api/QuanLyDatVe/LayDanhSachPhongVe?MaLichChieu=${maLichChieu}`,
                 method: 'GET'
             })
-            console.log();
+            console.log('datathongtinphongve', data);
             // Thanh cong lay du lieu ve cap nhat vao thongTinPhongVe
-            if(status === 200){
+            if (status === 200) {
                 dispatch({
                     type: THONG_TIN_PHONG_VE_ACTION,
                     thongTinPhongVe: data
@@ -66,6 +68,29 @@ export const layThongTinPhongVeApiAction = (maLichChieu) => {
             }
         } catch (err) {
             console.log(err)
+        }
+    }
+}
+
+
+export const datVeApiAction = async (thongTinVe) => {
+    return async (dispatch) => {
+        try {
+            let { data, status } = await Axios({
+                url: 'https://movie0706.cybersoft.edu.vn/api/QuanLyDatVe/DatVe',
+                method: 'POST',
+                data: thongTinVe,
+                headers: { 'Authorization': 'Bearer ' + localStorage.getItem(ACCESSTOKEN) }
+            })
+            dispatch(await layThongTinPhongVeApiAction(thongTinVe.maLichChieu))
+            Swal.fire('Thông báo', 'Đặt vé thành công', 'success')
+            console.log(data)
+            dispatch({
+                type: 'DAT_VE_THANH_CONG'
+            })
+        } catch (err) {
+            console.log(err)
+            Swal.fire('Thông báo', 'Đặt vé thất bại', 'error')
         }
     }
 }
